55f729c810dba793f224206a70cf271088620e61
Dibit Sha <dibitsha6@gmail.com>
2025-09-08 06:18:29 +0000

    remove the changes i have made earlier

--- src/app/dashboard/profile/page.tsx
+++ src/app/dashboard/profile/page.tsx
@@ -230,7 +230,6 @@ export default function ProfilePage() {
                             <Input
                               placeholder="Start typing your address..."
                               {...field}
-                              value={field.value ?? ''}
                               onChange={handleAddressChange}
                               onBlur={() => setTimeout(() => setIsSuggestionsVisible(false), 150)}
                               autoComplete="off"
@@ -274,7 +273,7 @@ export default function ProfilePage() {
                     <FormItem>
                       <FormLabel>Age</FormLabel>
                       <FormControl>
-                        <Input type="number" placeholder="Your age" {...field} value={field.value ?? ''}/>
+                        <Input type="number" placeholder="Your age" {...field} />
                       </FormControl>
                       <FormMessage />
                     </FormItem>
@@ -287,7 +286,7 @@ export default function ProfilePage() {
                     <FormItem>
                       <FormLabel>Weight (kg)</FormLabel>
                       <FormControl>
-                        <Input type="number" placeholder="Your weight in kilograms" {...field} value={field.value ?? ''}/>
+                        <Input type="number" placeholder="Your weight in kilograms" {...field} />
                       </FormControl>
                       <FormMessage />
                     </FormItem>
@@ -300,7 +299,7 @@ export default function ProfilePage() {
                     <FormItem>
                       <FormLabel>Height (cm)</FormLabel>
                       <FormControl>
-                        <Input type="number" placeholder="Your height in centimeters" {...field} value={field.value ?? ''} />
+                        <Input type="number" placeholder="Your height in centimeters" {...field} />
                       </FormControl>
                       <FormMessage />
                     </FormItem>
@@ -312,7 +311,7 @@ export default function ProfilePage() {
                   render={({ field }) => (
                     <FormItem>
                       <FormLabel>Blood Group</FormLabel>
-                      <Select onValueChange={field.onChange} value={field.value ?? ''}>
+                      <Select onValueChange={field.onChange} value={field.value}>
                         <FormControl>
                           <SelectTrigger>
                             <SelectValue placeholder="Select your blood group" />

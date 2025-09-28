f79df70edcd18b784742107fd16d720b940f1446
Dibit Sha <dibitsha6@gmail.com>
2025-09-28 07:25:22 +0000

    Try fixing this error: `Runtime Error: FirebaseError: Firebase: Error (a

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -8,8 +8,7 @@ const firebaseConfig = {
   projectId: "suraksha-jal-2-0",
   appId: "1:389862731723:web:0b7f8c5b0c473133f30965",
   storageBucket: "suraksha-jal-2-0.appspot.com",
-  // IMPORTANT: Replace this with your actual Firebase Web API Key
-  apiKey: process.env.AIzaSyBpZyT72vkbRUCR4_WyKn30xx6DEsWFC6Y,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: "suraksha-jal-2-0.firebaseapp.com",
   messagingSenderId: "389862731723",
 };
